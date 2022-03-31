0a447bf89eda763455c21c1a7a945a5b7fc8ce87
kiki0805 <kiki_hzz@163.com>
2022-04-01 01:17:30 +0200

    toggle light

--- Assets/Scripts/LightingSwitch.cs
+++ Assets/Scripts/LightingSwitch.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class LightingSwitch : MonoBehaviour
 {
+    public GameObject Switch;
+    public GameObject EmissiveObject;
+    Vector3 eulerRotation;
+    Color onColor = new Color(191f / 255f, 156f / 255f, 120f / 255f);
+    Color offColor = new Color(16f / 255f, 13f / 255f, 10f / 255f);
+    Material material;
     // Start is called before the first frame update
     void Start()
     {
-        // collider = gameObject.GetComponent<Collder>();
+        if (Switch != null)
+        {
+            eulerRotation = Switch.transform.rotation.eulerAngles;
+        }
+        if (EmissiveObject != null)
+        {
+            material = EmissiveObject.GetComponent<Renderer>().materials[1];
+        }
     }
 
     // Update is called once per frame
@@ -17,6 +30,30 @@ public class LightingSwitch : MonoBehaviour
     }
 
     public void ToggleEnabled() {
-        GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        bool lightEnabled = GetComponent<Light>().enabled;
+        if (!lightEnabled) {
+            if (Switch != null)
+            {
+                Switch.transform.rotation = Quaternion.Euler(8.5f, eulerRotation.y, eulerRotation.z);
+            }
+            if (EmissiveObject != null)
+            {
+                Debug.Log(material.GetColor("_EmissionColor"));
+                material.SetColor("_EmissionColor", onColor);
+            }
+        }
+        else
+        {
+            if (Switch != null)
+            {
+                Switch.transform.rotation = Quaternion.Euler(-8.5f, eulerRotation.y, eulerRotation.z);
+            }
+            if (EmissiveObject != null)
+            {
+                Debug.Log(material.GetColor("_EmissionColor"));
+                material.SetColor("_EmissionColor", offColor);
+            }
+        }
+        GetComponent<Light>().enabled = !lightEnabled;
     }
 }
