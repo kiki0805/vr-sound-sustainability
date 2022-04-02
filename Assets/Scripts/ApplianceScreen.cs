f85949b3b7f845e86e61db73e1c43ebfa707fabb
kiki0805 <kiki_hzz@163.com>
2022-04-02 18:14:42 +0200

    resolve conflict with package

--- Assets/Scripts/ApplianceScreen.cs
+++ Assets/Scripts/ApplianceScreen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Screen : MonoBehaviour
+public class ApplianceScreen : MonoBehaviour
 {
     public GameObject EmissiveScreen;
     public bool ScreenPowerOn = false;
