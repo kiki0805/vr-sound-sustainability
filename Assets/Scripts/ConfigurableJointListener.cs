cfb9345af528aed3083df71b5e52b975997bb1e6
kiki0805 <kiki_hzz@163.com>
2022-04-05 18:17:40 +0200

    drawer event listener

--- Assets/Scripts/ConfigurableJointListener.cs
+++ Assets/Scripts/ConfigurableJointListener.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+public enum PositionAxis
+{
+    X,
+    Y,
+    Z,
+}
+
 public class ConfigurableJointListener : MonoBehaviour
 {
     public UnityEvent ReachMinLimit;
@@ -15,6 +22,10 @@ public class ConfigurableJointListener : MonoBehaviour
     float openPosition;
     float lastPosition;
     bool moving;
+    public bool DebugOn = false;
+    public PositionAxis Axis = PositionAxis.Z;
+    public float repeatRate = 0.5f;
+    private float timer = 0;
 
     ConfigurableJoint joint;
 
@@ -22,48 +33,117 @@ public class ConfigurableJointListener : MonoBehaviour
     void Start()
     {
         joint = GetComponent<ConfigurableJoint>();
-        lastPosition = (float)transform.position.z;
+        lastPosition = currentPosition;
         closePosition = (float)lastPosition;
-        openPosition = (float)lastPosition + 0.4f;
-        Debug.Log($"closePosition {closePosition}");
-        Debug.Log($"openPosition {openPosition}");
+        openPosition = (float)lastPosition - 0.4f;
+        if (DebugOn)
+        {
+            Debug.Log($"closePosition {closePosition}");
+            Debug.Log($"openPosition {openPosition}");
+        }
+    }
+
+    float currentPosition
+    {
+        get
+        {
+            switch (Axis)
+            {
+                case PositionAxis.X:
+                    return (float)transform.position.x;
+                case PositionAxis.Y:
+                    return (float)transform.position.y;
+                case PositionAxis.Z:
+                    return (float)transform.position.z;
+                default:
+                    return (float)transform.position.z;
+            }
+        }
+    }
+
+    float currentVelocity
+    {
+        get
+        {
+            switch (Axis)
+            {
+                case PositionAxis.X:
+                    return GetComponent<Rigidbody>().velocity.x;
+                case PositionAxis.Y:
+                    return GetComponent<Rigidbody>().velocity.y;
+                case PositionAxis.Z:
+                    return GetComponent<Rigidbody>().velocity.z;
+                default:
+                    return GetComponent<Rigidbody>().velocity.z;
+            }
+        }
+    }
+
+    bool AlmostEqual(float v1, float v2)
+    {
+        return Mathf.Abs(v1 - v2) <= 0.001;
     }
 
     // Update is called once per frame
-    void Update()
+    void CheckState()
     {
-        Debug.Log($"velocity {GetComponent<Rigidbody>().velocity.z}");
-        Debug.Log($"position {transform.position.z}");
+        if (DebugOn)
+        {
+            // Debug.Log($"velocity {currentVelocity}");
+            Debug.Log($"cur, last position {currentPosition} {lastPosition}");
+            Debug.Log(Mathf.Round(currentPosition - openPosition));
+            Debug.Log(Mathf.Round(currentPosition - closePosition));
+        }
 
-        if(Mathf.Round(transform.position.z - closePosition) == 0 && Mathf.Round(lastPosition - closePosition) != 0)
+        if (AlmostEqual(currentPosition, closePosition) && !AlmostEqual(lastPosition, closePosition))
         {
             ReachMinLimit.Invoke();
+            if (DebugOn)
+            {
+                Debug.Log("Reach min");
+                
+            }
         }
-        if(Mathf.Round(transform.position.z - openPosition) == 0 && Mathf.Round(lastPosition - openPosition) != 0)
+        if (AlmostEqual(currentPosition, openPosition) && !AlmostEqual(lastPosition, openPosition))
         {
             ReachMaxLimit.Invoke();
         }
-        if (Mathf.Round(lastPosition - openPosition) == 0 && Mathf.Round(transform.position.z - lastPosition) != 0)
+        if (AlmostEqual(lastPosition, openPosition) && !AlmostEqual(lastPosition, currentPosition))
         {
             DetachMaxLimit.Invoke();
         }
-        if (Mathf.Round(closePosition) == 0 && Mathf.Round(transform.position.z - lastPosition) != 0)
+        if (AlmostEqual(lastPosition, closePosition) && !AlmostEqual(lastPosition, currentPosition))
         {
             DetachMinLimit.Invoke();
+            if (DebugOn)
+            {
+                Debug.Log("Detach min");
+            }
         }
-        if (Mathf.Round(lastPosition - transform.position.z) != 0)
+        if (!Mathf.Approximately(lastPosition, currentPosition))
         {
-            lastPosition = (float)transform.position.z;
+            lastPosition = currentPosition;
         }
-        if (GetComponent<Rigidbody>().velocity.z > 5 && !moving)
+        if (currentVelocity > 0 && !moving)
         {
             moving = true;
             StartMove.Invoke();
+            if (DebugOn) Debug.Log("Start move");
         }
-        if (GetComponent<Rigidbody>().velocity.z < 5 && moving)
+        if (currentVelocity == 0 && moving)
         {
             moving = false;
             StopMove.Invoke();
         }
+    }
+
+    void Update()
+    {
+        if (timer < 0)
+        {
+            CheckState();
+            timer = repeatRate;
+        }
+        timer -= Time.deltaTime;
     }
 }
