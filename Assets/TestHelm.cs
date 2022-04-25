7d8783b9536721aec54b520a25c04cbf0044c259
kiki0805 <kiki_hzz@163.com>
2022-04-26 00:18:11 +0200

    remove unused sounds

---
+++ Assets/TestHelm.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHelm : MonoBehaviour
+{
+    AudioHelm.HelmController controller;
+    public int midiNote = 72;
+    public bool trigger = false;
+    public bool stopTrigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<AudioHelm.HelmController>();
+        // controller.NoteOn(midiNote, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger) {
+            controller.NoteOff(midiNote);
+            controller.NoteOn(midiNote);
+            trigger = false;
+        }
+        if (stopTrigger) {
+            controller.NoteOff(midiNote);
+            stopTrigger = false;
+        }
+    }
+}
