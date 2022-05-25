fc2224e3c87fa575c0ce0899f5791ebc806b3dbb
Zezhe Huang <kiki_hzz@163.com>
2022-05-25 10:06:20 +0200

    tooltips

---
+++ Assets/Scripts/Cheater.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cheater : MonoBehaviour
+{
+    public TMP_Text tooltip;
+    private int referCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTooltip(string tooltipStr) {
+        tooltipStr = tooltipStr.Replace("\\n", "\r\n");
+        tooltip.text = tooltipStr;
+        referCount ++;
+    }
+
+    public void ClearTooltip() {
+        referCount --;
+        if (referCount == 0) {
+            tooltip.text = "";
+        }
+    }
+}
